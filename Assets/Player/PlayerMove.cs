4a191742a6ee2e6233b5dd80290faa41c817d192
Matthew Chase <antishow@gmail.com>
2021-10-28 07:12:13 -0400

    Add new input system
    Just a quick WASD to  make sure its all connected

---
+++ Assets/Player/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour
+{
+	private CharacterController characterController;
+	private Vector2 input;
+	private Vector3 velocity;
+
+	public float playerSpeed = 5f;
+
+	void Awake()
+	{
+		characterController = GetComponent<CharacterController>();
+	}
+
+	void OnMove(InputValue value)
+	{
+		input = value.Get<Vector2>();
+	}
+
+	void Update()
+	{
+		if (characterController.isGrounded && velocity.y < 0)
+		{
+			velocity.y = 0f;
+		}
+
+		Vector3 move = transform.right * input.x + transform.forward * input.y;
+		characterController.Move(move * Time.deltaTime * playerSpeed);
+		velocity.y += -Physics.gravity.magnitude * Time.deltaTime;
+		characterController.Move(velocity * Time.deltaTime);
+	}
+}
